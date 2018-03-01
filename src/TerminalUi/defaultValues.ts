98e3af5168c4b9fff6dce57cb497a0707693b32b
danikaze <danikaze@gmail.com>
2018-03-01 23:51:16 +0900

    Added TerminalUI (wip)

---
+++ src/TerminalUi/defaultValues.ts
@@ -0,0 +1,23 @@
+import { BoxBorderCharacters } from './TerminalUi';
+
+// export const boxBorderCharacters: BoxBorderCharacters = {
+//   topLeft: '╔',
+//   top: '═',
+//   topRight: '╗',
+//   left: '║',
+//   right: '║',
+//   bottomLeft: '╚',
+//   bottom: '═',
+//   bottomRight: '╝',
+// };
+
+export const boxBorderCharacters: BoxBorderCharacters = {
+  topLeft: '┌',
+  top: '─',
+  topRight: '┐',
+  left: '│',
+  right: '│',
+  bottomLeft: '└',
+  bottom: '─',
+  bottomRight: '┘',
+};

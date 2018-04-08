e30cf496af351a531998694c1985293064c5daa6
danikaze <danikaze@gmail.com>
2018-04-08 21:22:54 +0900

    assignCharStyle doesnt fail if no style is provided

--- src/util/assignCharStyle.ts
+++ src/util/assignCharStyle.ts
@@ -9,20 +9,22 @@ import { CharStyle } from '../Terminal';
  * @returns `target`
  */
 export function assignCharStyle(target: CharStyle, source: CharStyle): CharStyle {
-  if (source.font) {
-    target.font = source.font;
-  }
-  if (source.fontOffsetX) {
-    target.fontOffsetX = source.fontOffsetX;
-  }
-  if (source.fontOffsetY) {
-    target.fontOffsetY = source.fontOffsetY;
-  }
-  if (source.fg) {
-    target.fg = source.fg;
-  }
-  if (source.bg) {
-    target.bg = source.bg;
+  if (source) {
+    if (source.font) {
+      target.font = source.font;
+    }
+    if (source.fontOffsetX) {
+      target.fontOffsetX = source.fontOffsetX;
+    }
+    if (source.fontOffsetY) {
+      target.fontOffsetY = source.fontOffsetY;
+    }
+    if (source.fg) {
+      target.fg = source.fg;
+    }
+    if (source.bg) {
+      target.bg = source.bg;
+    }
   }
 
   return target;
